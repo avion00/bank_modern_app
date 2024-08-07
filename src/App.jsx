5dc42f01daf0bfb74c88a4631d1a3336519f3a31
Abhishek kumar chaudhary <avioncompany00@gmail.com>
2024-08-08 00:00:12 +0545

    no change

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,7 @@ function App() {
     <div className="bg-zinc-800 h-screen w-full text-sky-100 font-semibold p-4">
       <div className="w-fit border border-rose-100 rounded-md p-3 hover:bg-zinc-600 duration-300 ease-in-out m-2">
         hello my name is abhi Lorem ipsnseicing elit. Laborum dolor assumenda
-        nihil fugit reiciendis. Voluptatem, ab doloribus. Aspernatur esse
-        corporis illo laborum ab temporibus doloremque atque sunt iusto,
-        exercitationem ea.
+        nihil fugit res doloremque atque sunt iusto, exercitationem ea.
       </div>
     </div>
   );
