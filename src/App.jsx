72455bc922b41fc2c53fb3bb7929ce6723f24151
Abhishek kumar chaudhary <avioncompany00@gmail.com>
2024-08-07 23:59:49 +0545

    showafasdfasdfasdfaasdfasdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div className='bg-zinc-800 h-screen w-full text-sky-100 font-semibold p-4'>
-      <div className='w-fit border border-rose-100 rounded-md p-3 hover:bg-zinc-600 duration-300 ease-in-out m-2'>
-        hello my name is abhi Lorem ipsnsectetur adipisicing elit. Laborum dolor assumenda nihil fugit reiciendis. Voluptatem, ab doloribus. Aspernatur esse corporis illo laborum ab temporibus doloremque atque sunt iusto, exercitationem ea.
+    <div className="bg-zinc-800 h-screen w-full text-sky-100 font-semibold p-4">
+      <div className="w-fit border border-rose-100 rounded-md p-3 hover:bg-zinc-600 duration-300 ease-in-out m-2">
+        hello my name is abhi Lorem ipsnseicing elit. Laborum dolor assumenda
+        nihil fugit reiciendis. Voluptatem, ab doloribus. Aspernatur esse
+        corporis illo laborum ab temporibus doloremque atque sunt iusto,
+        exercitationem ea.
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
