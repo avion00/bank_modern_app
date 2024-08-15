b8f95ea868f85f00a22c00b7bd70b454a97f9cfd
Abhishek kumar chaudhary <avioncompany00@gmail.com>
2024-08-15 23:06:18 +0545

    first

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,12 @@ const App = () => (
       </div>
     </div>
 
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero/>
+      </div>
+    </div>
+
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
